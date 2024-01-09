a60ffde75cc715fc6c1b96177307366e4f5ac0df
Marc Duiker <marcduiker@users.noreply.github.com>
2024-01-09 16:28:14 +0100

    Add method to start workflow

---
+++ back-end/PizzaOrderService/Controllers/WorkflowController.cs
@@ -0,0 +1,49 @@
+using Dapr;
+using Dapr.Workflow;
+using Microsoft.AspNetCore.Mvc;
+using OrderService.Workflows;
+using Shared.Models;
+
+namespace CheckoutService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WorkflowController : ControllerBase
+{
+    private readonly ILogger<InventoryController> _logger;
+    
+    private readonly DaprWorkflowClient _daprWorkflowClient;
+
+    public WorkflowController(ILogger<InventoryController> logger, DaprWorkflowClient daprWorkflowClient)
+    {
+        _logger = logger;
+        _daprWorkflowClient = daprWorkflowClient;
+    }
+
+    [HttpPost("orderReceived")]
+    [Topic("pizza-pubsub", "received-orders")]
+    public async Task<IResult> StartPizzaWorkflow([FromBody] Order order)
+    {
+        _logger.LogInformation($"Received message to start pizza workflow with ID {order.OrderId}.");
+        await _daprWorkflowClient.ScheduleNewWorkflowAsync(
+            nameof(PizzaOrderWorkflow),
+            order.OrderId,
+            order);
+
+        return Results.Ok();
+    }
+
+    [HttpPost("orderPrepared")]
+    [Topic("pizza-pubsub", "prepared-orders")]
+    public async Task<IResult> RaiseOrderPreparedEvent([FromBody] Order order)
+    {
+        var instanceId = order.OrderId;
+        _logger.LogInformation($"Received message to raise order-prepared event for {instanceId}.");
+        await _daprWorkflowClient.RaiseEventAsync(
+            instanceId,
+            "order-prepared",
+            true);
+
+        return Results.Ok();
+    }
+}
