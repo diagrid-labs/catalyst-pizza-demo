c90a44fcf3a4345272fc58f2938e018592107d4e
Marc Duiker <marcduiker@users.noreply.github.com>
2024-01-29 15:05:01 +0100

    Add devcontainer
    Signed-off-by: Marc Duiker <marcduiker@users.noreply.github.com>

---
+++ scripts/install.sh
@@ -0,0 +1,11 @@
+
+sudo apt-get update
+sudo apt-get install -y xdg-utils
+dotnet build ./back-end/PizzaOrderService
+dotnet build ./EvilCorpDemo/KitchenService
+cd ./front-end
+npm install
+cd ..
+npm i -g vercel
+curl -o- https://downloads.diagrid.io/cli/install-catalyst.sh | bash
+sudo mv ./diagrid /usr/local/bin 
