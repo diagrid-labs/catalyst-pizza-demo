a140a56068fc248c44c7bba94910528ef3766198
Marc Duiker <marcduiker@users.noreply.github.com>
2024-09-25 16:55:44 +0200

    Update devcontainer

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,9 +1,8 @@
-printf "\n\nHold on, we're installing some tools and building the projects...\n\n"
 sudo apt-get update
 sudo apt-get install -y xdg-utils
 dotnet build ./back-end/PizzaOrderService
-dotnet build ./EvilCorpDemo/KitchenService
-cd ./front-end
+dotnet build ./back-end/KitchenService
+cd ../front-end
 npm install
 cd ..
 npm i -g vercel
